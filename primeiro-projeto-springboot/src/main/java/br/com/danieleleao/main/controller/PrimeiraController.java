9cb4a6df36c740483dc2ac998108345a8b62c4d1
Carlos Henrique <ichfs9108@gmail.com>
2023-10-27 00:02:57 -0300

    RockSeat: Path Params

--- primeiro-projeto-springboot/src/main/java/br/com/danieleleao/main/controller/PrimeiraController.java
+++ primeiro-projeto-springboot/src/main/java/br/com/danieleleao/main/controller/PrimeiraController.java
@@ -1,15 +1,26 @@
 package br.com.danieleleao.main.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
 
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo() {
-        return "Meu primeiro metodo api rest";
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id) {
+        return "O paramêtro é: " + id;
     }
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoCOmQueryParams(@RequestParam String id) {
+        return "O paramêtro com metodoComQueryParams é: " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoCOmQueryParams2(@RequestParam Map<String, String> allParams) {
+        return "O paramêtro com metodoComQueryParams é: " + allParams.entrySet();
+    }
+
 }
